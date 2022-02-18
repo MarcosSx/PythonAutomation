78c8ffca1318303aee08acf441628c4cb77bf57c
Marcos <marcosxavier03@gmail.com>
2022-02-18 18:05:19 -0300

    Adding more datas to test_e2e

---
+++ PythonSelFramework/TestData/TestE2EData.py
@@ -0,0 +1,3 @@
+class TestE2EData:
+    test_e2e_data = {'product': 'iphone X', 'search': 'United', 'country': 'United Kingdom', 'success': 'Success!',
+                     'failure': 'Failure'}
--- PythonSelFramework/tests/test_e2e.py
+++ PythonSelFramework/tests/test_e2e.py
@@ -1,3 +1,6 @@
+import pytest
+
+from TestData.TestE2EData import TestE2EData
 from pageObjects.CheckoutPage import CheckoutPage
 from pageObjects.ConfirmPage import ConfirmPage
 from pageObjects.HomePage import HomePage
@@ -6,7 +9,7 @@ from utilities.BaseClass import BaseClass
 
 class TestOne(BaseClass):
 
-    def test_e2e(self, setup):
+    def test_e2e(self, setup, get_data):
         home_page = HomePage(self.driver)
         checkout_page = CheckoutPage(self.driver)
         confirm_page = ConfirmPage(self.driver)
@@ -17,24 +20,29 @@ class TestOne(BaseClass):
             i += 1
             card_text = card.text
             print(card_text)
-            if card_text in 'iphone X':
+            if card_text in get_data['product']:
                 checkout_page.getCardFooter()[i].click()
         checkout_page.getBtnCheckout().click()
 
         confirm_page.getBtnCheckout().click()
-        confirm_page.getLblCountry().send_keys('united')
-        self.verifyLinkPresence('United Kingdom')
+        confirm_page.getLblCountry().send_keys(get_data['search'])
+        self.verifyLinkPresence('country')
         confirm_page.getOptCountry().click()
         confirm_page.getCbkIAgree().click()
         confirm_page.getBtnPurchase().click()
         success = confirm_page.getLblSuccess().text
 
+        # not defined yet screenschot method
         ok = ''
         try:
-            assert 'Success!' in success
-            ok = 'Success'
+            assert get_data['success'] in success
+            ok = get_data['success']
         except:
-            print('Failure')
-            ok = 'Failure'
+            print(get_data['failure'])
+            ok = get_data['failure']
         finally:
             self.driver.get_screenshot_as_file(f'{ok}.png')
+
+    @pytest.fixture(params=TestE2EData.test_e2e_data)
+    def get_data(self, request):
+        return request.param
