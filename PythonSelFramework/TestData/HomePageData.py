15b6a3374919690a708abf0fbf0c38faf2967618
Marcos <marcosxavier03@gmail.com>
2022-02-25 18:50:47 -0300

    Integrating Exel utility into the framework

--- PythonSelFramework/TestData/HomePageData.py
+++ PythonSelFramework/TestData/HomePageData.py
@@ -2,8 +2,6 @@ import openpyxl
 
 
 class HomePageData:
-    test_HomePageData = [{'firstName': 'Marcos', 'email': 'marcos@email.com', 'gender': 'Male'},
-                         {'firstName': 'Beatriz', 'email': 'beatriz@email.com', 'gender': 'Female'}]
 
     @staticmethod
     def getTestData(test_case_name):
